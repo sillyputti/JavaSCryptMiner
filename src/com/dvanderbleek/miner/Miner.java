31093dbf6dbbf483a3a032bbfe8ebff289fccc94
Alistair Buxton <a.j.buxton@gmail.com>
2011-11-18 17:50:25 +0000

    Clean up and remove unneccessary code.

--- src/com/dvanderbleek/miner/Miner.java
+++ src/com/dvanderbleek/miner/Miner.java
@@ -18,7 +18,6 @@ import com.lambdaworks.crypto.SCrypt;
 
 
 public class Miner {
-
 	/**
 	 * @param args
 	 * @throws GeneralSecurityException 
@@ -34,80 +33,66 @@ public class Miner {
 		          return new PasswordAuthentication (rpcuser, rpcpassword.toCharArray());
 		      }
 		  });
-		
+		  
+		while(true) {
 		  Work work = getwork(); //Gets the work from the server
 		  String data = work.result.data; //Gets the data to hash from the work
 		  String target = work.result.target;//Gets the target from the work
-		   
-		  //This chunk pulls apart the data so they can be endian switched (see the scrypt proof of work page on the wiki)
-		  String version = data.substring(0, 8);
-		  String prevhash = data.substring(8, 72);
-		  String merkle = data.substring(72, 136 );
-		  String timestamp = data.substring(136, 144);
-		  String bits = data.substring(144, 152);
-		  String nonce = data.substring(152,160);
-		   
-		  //This chunk creates endian switched byte arrays from the data
-		   byte[] versionbit = myEndianSwitch(Converter.fromHexString(version));
-		   byte[] prevhashbit = myEndianSwitch(Converter.fromHexString(prevhash));
-		   byte[] merklebit = myEndianSwitch(Converter.fromHexString(merkle));
-		   byte[] timestampbit = myEndianSwitch(Converter.fromHexString(timestamp));
-		   byte[] bitsbit = myEndianSwitch(Converter.fromHexString(bits));
-		   byte[] noncebit = myEndianSwitch(Converter.fromHexString(nonce));
-		   
-	       //This chunk of code reassembles the data into a single byre array      
-		   byte[] databyte = new byte[80];
-		   System.arraycopy(versionbit, 0, databyte, 0, versionbit.length);
-		   System.arraycopy(prevhashbit, 0, databyte, 4, prevhashbit.length);
-		   System.arraycopy(merklebit, 0, databyte, 36, merklebit.length);
-		   System.arraycopy(timestampbit, 0, databyte, 68, timestampbit.length);
-		   System.arraycopy(bitsbit, 0, databyte, 72, bitsbit.length);
-		   System.arraycopy(noncebit, 0, databyte, 76, noncebit.length);
-		   
-		   //Converts the target string to a byte array for easier comparison
+		  String realdata = data.substring(0, 160);
+		  byte[] databyte = endianSwitch(Converter.fromHexString(realdata));
+		  
+     	   //Converts the target string to a byte array for easier comparison
 		   byte[] targetbyte = Converter.fromHexString(target);
-		   targetbyte = endianSwitch(targetbyte);
-
-		   System.out.println(myPrintByteArray(databyte));
-		   
-		   byte[] scrypted = doScrypt(databyte, targetbyte);//Calls sCrypt with the proper parameters, and returns the correct data
-		   
-		   work.result.data = printByteArray(scrypted);
-		   System.out.println(sendWork(work));//Send the work
+		   System.out.println(printByteArray(targetbyte));
+		   if (doScrypt(databyte, targetbyte)) { //Calls sCrypt with the proper parameters, and returns the correct data
+    		   work.result.data = printByteArray(endianSwitch(databyte))+data.substring(160, 256);
+	    	   System.out.println(sendWork(work));//Send the work
+		   }
+		}
 	}
 	
-	public static byte[] doScrypt(byte[] databyte, byte[] target) throws GeneralSecurityException{
+	public static boolean doScrypt(byte[] databyte, byte[] target) throws GeneralSecurityException{
 		//Initialize the nonce
-		byte[] nonce = new byte[4];
-		nonce[0] = databyte[76] ;
-		nonce[1] = databyte[77] ;
-		nonce[2] = databyte[78] ;
-		nonce[3] = databyte[79] ;
+		int[] nonce = new int[4];
+		nonce[3] = databyte[76] ;
+		nonce[2] = databyte[77];
+		nonce[1] = databyte[78] ;
+		nonce[0] = databyte[79] ;
+		int ns = 0;
+		int cc;
 		boolean found = false;
 		//Loop over and increment nonce
-		while(!found){
+		while(true){
 			//Set the bytes of the data to the nonce
-			databyte[76] = nonce[0];
-			databyte[77] = nonce[1];
-			databyte[78] = nonce[2];
-			databyte[79] = nonce[3];
+			databyte[76] = (byte) (nonce[3]&0xff);
+			databyte[77] = (byte) (nonce[2]&0xff);
+			databyte[78] = (byte) (nonce[1]&0xff);
+			databyte[79] = (byte) (nonce[0]&0xff);
 			
 			byte[] scrypted = (SCrypt.scryptJ(databyte,databyte, 1024, 1, 1, 32));//Scrypt the data with proper params
 			
-			BigInteger bigScrypt = new BigInteger(printByteArray(endianSwitch(scrypted)), 16); //Create a bigInteger to compare against the target
-			BigInteger bigTarget = new BigInteger(printByteArray(target),16);//Create a bigInteger from the target 
-			if(bigScrypt.compareTo(bigTarget) == -1) return databyte;//Compare the two bigIntegers, return the data with nonce if smaller
+			if (scrypted[31] == 0) {
+				cc = 30;
+				while((cc>=0) && (scrypted[cc] == 0)) cc -= 1;
+				if((cc>0) && (((0x100+scrypted[cc])&0xff) < ((0x100+target[cc])&0xff))) {
+					System.out.println(printByteArray(scrypted));
+					return true;
+				}
+			}
 			
-			else incrementAtIndex(nonce, nonce.length-1); //Otherwise increment the nonce
-			
+			incrementAtIndex(nonce, nonce.length-1); //Otherwise increment the nonce
+			ns += 1;
+			if(ns>0x4fff) {
+				System.out.println("giving up on block\n");
+				return false;
+			}
 		}
-		return databyte;
 	}
 	
 	
-	public  static void  incrementAtIndex(byte[] array, int index) {
+	public  static void  incrementAtIndex(int[] array, int index) {
 		//Short method to increment the nonce
-	    if (array[index] == Byte.MAX_VALUE) {
+	    if (array[index] == 255) {
 	        array[index] = 0;
 	        if(index > 0)
 	            incrementAtIndex(array, index - 1);
@@ -118,19 +103,6 @@ public class Miner {
 	}
 	
 	
-	public static String myPrintByteArray(byte[] bites){
-		//Method to convert a byte array to hex literal
-		String str = "";
-		int n = 0;
-		for(byte bite:bites){
-			n += 1;
-			str = str + (Integer.toString( ( bite & 0xff ) + 0x100, 16 /* radix */ ).substring( 1 )) + " ";
-			if((n%16) == 0) { str += "\n"; }
-			else if((n%4) == 0) { str += " "; }
-		}
-		return str;
-	}
-
 	public static String printByteArray(byte[] bites){
 		//Method to convert a byte array to hex literal
 		String str = "";
@@ -141,15 +113,6 @@ public class Miner {
 	}
 	
 	public static byte[] endianSwitch(byte[] bytes) {
-		//Method to switch the endianess of a byte array
-	   byte[] bytes2 = new byte[bytes.length];
-	   for(int i = 0; i < bytes.length;  i++){
-		   bytes2[i] = bytes[bytes.length-i-1];
-	   }
-	   return bytes2;
-	}
-
-	public static byte[] myEndianSwitch(byte[] bytes) {
 		//Method to switch the endianess of a byte array
 	   byte[] bytes2 = new byte[bytes.length];
 	   for(int i = 0; i < bytes.length;  i+=4){
@@ -165,11 +128,11 @@ public class Miner {
 	public static Work getwork() throws IOException{
 		//Method to getwork
 		URL url = new URL("http://127.0.0.1:9332");
-	    URLConnection conn = url.openConnection();
+		URLConnection conn = url.openConnection();
 	    conn.setDoOutput(true);
 	    conn.setDoInput(true);
 	    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-	    String rpcreturn = "{\"jsonrpc\": \"1.0\" , \"method\": \"getwork\" }";//JSON RPC call for getting work
+	    String rpcreturn = "{ \"method\": \"getwork\", \"id\" : 1 }";//JSON RPC call for getting work
 	    wr.write(rpcreturn);
 	    wr.flush();
 	    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -185,12 +148,12 @@ public class Miner {
 	public static String sendWork(Work work) throws IOException{
 		//Very similar to getwork method
 		URL url = new URL("http://127.0.0.1:9332");
-	    URLConnection conn = url.openConnection();
+		URLConnection conn = url.openConnection();
 	    conn.setDoOutput(true);
 	    conn.setDoInput(true);
 	    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
 	    System.out.println(work.result.data);
-	    String rpcreturn = "{\"jsonrpc\": \"1.0\" , \"method\": \"getwork\" , \"params\" : [\"" +work.result.data+ "\"]}";//RPC call with the new nonced data
+	    String rpcreturn = "{ \"method\": \"getwork\", \"params\" : [ \"" +work.result.data+ "\" ], \"id\" : 1 }";//RPC call with the new nonced data
 	    System.out.println(rpcreturn);
 	    wr.write(rpcreturn);
 	    wr.flush();
@@ -198,6 +161,6 @@ public class Miner {
 	    String line;
 	    line = rd.readLine();
 	    rd.close();
-	return line;
+	    return line;
 	}
 }
